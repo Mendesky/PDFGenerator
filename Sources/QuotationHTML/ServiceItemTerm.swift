29e485ed641ffa3e3ede7ed176387f560f45ddef
Suling <sulingchen0123@gmail.com>
2024-09-30 16:31:51 +0800

    [UPDATE] serviceItemTerm's term type is not optional

--- Sources/QuotationHTML/ServiceItemTerm.swift
+++ Sources/QuotationHTML/ServiceItemTerm.swift
@@ -8,17 +8,15 @@
 import Plot
 
 public struct ServiceItemTerm: Component {
-    let term: String?
+    let term: String
     
     public var body: any Component{
         ComponentGroup{
-            if let term {
-                ListItem(term)
-            }
+            ListItem(term)
         }
     }
     
-    public init(term: String?) {
+    public init(term: String) {
         self.term = term
     }
 }
