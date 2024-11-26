5d050568b33f3e094ba063d1560777b93f7160f2
Suling <sulingchen0123@gmail.com>
2024-11-26 16:18:14 +0800

    [UPDATE] Organization rawValue

--- Sources/QuotationHTML/Organization.swift
+++ Sources/QuotationHTML/Organization.swift
@@ -3,12 +3,12 @@ import Foundation
 
 public enum Organization: String, Codable, Sendable {
     public static var `default`: Organization  { return .jw }
-    case jw = "01020314" // 嘉威
-    case jwTaipei = "88183980" // 嘉威-台北所
-    case jwTaoyuan = "41171816" // 嘉威-桃園所
-    case jwTaichung = "47575385" // 嘉威-台中所
-    case jwChanghua = "82576039" // 嘉威-彰化所
-    case jwChiayi = "47779732" // 嘉威-嘉義所
-    case kd = "34873876" // 康達
-    case jwipo = "54842985" // 捷達優
+    case jw = "嘉威聯合會計師事務所"
+    case jwTaipei = "嘉威聯合會計師事務所-台北所"
+    case jwTaoyuan = "嘉威聯合會計師事務所-桃園所"
+    case jwTaichung = "嘉威聯合會計師事務所-台中所"
+    case jwChanghua = "嘉威聯合會計師事務所-彰化所"
+    case jwChiayi = "嘉威聯合會計師事務所-嘉義所"
+    case kd = "康達會計師事務所"
+    case jwipo = "捷達優企管顧問股份有限公司"
 }
