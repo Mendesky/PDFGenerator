05753a6355cbd02de1a332f8f15361e84e06292b
Suling <sulingchen0123@gmail.com>
2024-11-27 15:38:14 +0800

    [UPDATE] organization displayName

--- Sources/QuotationHTML/Organization.swift
+++ Sources/QuotationHTML/Organization.swift
@@ -44,18 +44,8 @@ public enum Organization: String, Codable, Sendable {
 
     public var displayName: String {
         return switch self {
-        case .jw:
+        case .jw, .jwTaipei, .jwTaoyuan, .jwTaichung, .jwChanghua, .jwChiayi:
             "嘉威聯合會計師事務所"
-        case .jwTaipei:
-            "嘉威聯合會計師事務所-台北所"
-        case .jwTaoyuan:
-            "嘉威聯合會計師事務所-桃園所"
-        case .jwTaichung:
-            "嘉威聯合會計師事務所-台中所"
-        case .jwChanghua:
-            "嘉威聯合會計師事務所-彰化所"
-        case .jwChiayi:
-            "嘉威聯合會計師事務所-嘉義所"
         case .kd:
             "康達會計師事務所"
         case .jwipo:
