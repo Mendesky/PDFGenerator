c2ac165a770456b906d24e45d95b6d2abc7841cb
Grady Zhuo <gradyzhuo@jwcpas.com.tw>
2025-05-16 14:18:38 +0800

    [UPDATE] the font size of Payment and others.

--- Sources/QuotationHTML/ContractHeader.swift
+++ Sources/QuotationHTML/ContractHeader.swift
@@ -23,20 +23,20 @@ public struct ContractHeader: Component {
         ComponentGroup{
             Table{
                 TableRow{
-                    TableCell("受 文 者：").style("vertical-align: top; width: 6em;")
-                    TableCell("\(receiver)（以下簡稱 貴公司）")
+                    TableCell("受 文 者：").style("vertical-align: top; width: 6em; font-size: 1rem;")
+                    TableCell("\(receiver)（以下簡稱 貴公司）").style("font-size: 1rem;")
                 }
                 TableRow{
-                    TableCell("發 文 者：").style("vertical-align: top;")
-                    TableCell("\(sender.displayName)（以下簡稱 本事務所）")
+                    TableCell("發 文 者：").style("vertical-align: top; font-size: 1rem;")
+                    TableCell("\(sender.displayName)（以下簡稱 本事務所）").style("font-size: 1rem;")
                 }
                 TableRow{
-                    TableCell("主    旨：").style("vertical-align: top;")
-                    TableCell(subject)
+                    TableCell("主    旨：").style("vertical-align: top; font-size: 1rem;")
+                    TableCell(subject).style("font-size: 1rem;")
                 }
                 TableRow{
-                    TableCell("說    明：").style("vertical-align: top;")
-                    TableCell(content)
+                    TableCell("說    明：").style("vertical-align: top; font-size: 1rem;")
+                    TableCell(content).style("font-size: 1rem;")
                 }
             }.style("margin: 2rem 2rem 3rem 2rem;")
         }
