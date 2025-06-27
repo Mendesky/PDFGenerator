1ecf6f20e620048938ac589553a95ac0e15cc1f3
Grady Zhuo <gradyzhuo@jwcpas.com.tw>
2025-06-27 10:52:50 +0800

    [UPDATE] refactor ServiceItemTerm to ContractProvision instead.

--- Sources/QuotationHTML/QuotingServiceTerm.swift
+++ Sources/QuotationHTML/QuotingServiceTerm.swift
@@ -9,15 +9,15 @@ import Plot
 public struct QuotingServiceTerm {
     let title: String
     let term: String?
-    let serviceItemTerms: [ServiceItemTerm]?
+    let serviceItemTerms: [ContractProvision]?
     
-    public init(title: String, term: String?, serviceItemTerms: [ServiceItemTerm]?) {
+    public init(title: String, term: String?, serviceItemTerms: [ContractProvision]?) {
         self.title = title
         self.term = term
         self.serviceItemTerms = serviceItemTerms
     }
     
     public init(title: String, term: String?, termStrings: [String]) {
-        self.init(title: title, term: term, serviceItemTerms: termStrings.map{ ServiceItemTerm(term: $0)})
+        self.init(title: title, term: term, serviceItemTerms: termStrings.map{ ContractProvision(term: $0)})
     }
 }
