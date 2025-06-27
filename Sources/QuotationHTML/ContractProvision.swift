41a874e3cafb84d695083ed7849b41dcfd107819
Grady Zhuo <gradyzhuo@jwcpas.com.tw>
2025-06-27 10:50:24 +0800

    [UPDATE] add ContractSection and ContractProvision for RightsAndObligations.

---
+++ Sources/QuotationHTML/ContractProvision.swift
@@ -0,0 +1,15 @@
+//
+//  ContractProvision.swift
+//  PDFGenerator
+//
+//  Created by Grady Zhuo on 2025/6/27.
+//
+
+
+public struct ContractProvision{
+    let term: String
+    
+    public init(term: String) {
+        self.term = term
+    }
+}
